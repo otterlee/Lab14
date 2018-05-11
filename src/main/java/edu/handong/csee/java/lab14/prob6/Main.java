cf153793f0bdcd8db5b512174533ac0e932904cb
otterlee <21700525@handong.edu>
2018-05-11 21:59:52 +0900

    third

--- src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
@@ -7,8 +7,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner keyboard = new Scanner(System.in);//instantiate Scanner as keyboard
 		CustomId Id1 = new CustomId();//instantiate CustomId as Id1
-		while(true)//use loop
-			try {
+		while(true)//use loop to try continuously in cusomizing
+			try {//use try block to detect exception
 				if(Id1.tellStep() == 1) {//if step in customizing id is 1
 					System.out.print("Enter your name : ");//prompt to enter the name
 					String name = keyboard.nextLine();//take input string to name 
@@ -24,7 +24,7 @@ public class Main {
 					String race = keyboard.next();//take input string to race
 					Id1.setRace(race);//call method setRace to set race and to know this race is valid
 				}
-				break;
+				break;//if there is no probelm end this loop
 			} catch(Exception e) {//if there is exception
 				System.out.println(e.getMessage());//print-out there error message
 				continue;//and continue to custom id
